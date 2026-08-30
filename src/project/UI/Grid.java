e0085e60c4720678c9739a9e1ed410d40750fa38
AikenB <bouisaiken@gmail.com>
2026-08-29 20:58:43 -0700

    Co-authored-by: jverma8 <jverma8@users.noreply.github.com>

--- src/project/UI/Grid.java
+++ src/project/UI/Grid.java
@@ -2,4 +2,28 @@ package project.UI;
 
 public class Grid {
 
+    //in grid blocks not pixels
+    public final int MAP_WIDTH = 100;
+    public final int MAP_HEIGHT = 1000;
+    public final int[][] grid = new int[MAP_WIDTH][MAP_HEIGHT];
+
+    public Grid(){
+        initializeGrid();
+    }
+
+    public void initializeGrid(){
+        for(int i = 0; i < MAP_WIDTH; i++){
+            for(int j = 0; j < MAP_HEIGHT; j++){
+                grid[i][j] = 1;
+            }
+        }
+
+        //create spawn area
+        for(int i = MAP_WIDTH/2 - 2; i < MAP_WIDTH/2 + 2; i++){ // width gets 48, 49, 50, 51
+            for(int j = MAP_HEIGHT - 3; j < MAP_HEIGHT - 9; j++){ // height gets 987-990, 
+                grid[i][j] = 0;
+            }
+        }
+        
+    }
 }
--- src/project/gameobjects/Player.java
+++ src/project/gameobjects/Player.java
@@ -1,5 +1,5 @@
 package project.gameobjects;
 
 public class Player {
-
+    
 }
